83421a727c545452ddd18d2f02e7c8f4887b5ddd
Gaurav Naik <gauravnaik6288@gmail.com>
2016-04-15 23:55:29 +0530

    Second Commit

---
+++ kiratplastics_erpnext/config/desktop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Kirat Plastics ERPNext",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Kirat Plastics ERPNext")
+		}
+	]
