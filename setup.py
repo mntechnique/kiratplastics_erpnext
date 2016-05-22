e23d84282970dac4dc209f2f0da9222331e18b46
Revant Nandgaonkar <revant.one@gmail.com>
2016-05-22 13:39:15 +0600

    bumped to version 1.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-version = '1.0.5'
+version = '1.0.6'
 requirements = parse_requirements("requirements.txt", session="")
 
 setup(
