175f86e9f40912f1218b5e4b96b2d3f31edacdcc
Revant Nandgaonkar <revant.one@gmail.com>
2016-06-30 19:10:17 +0530

    Update Items from Excise Chapter

--- kiratplastics_erpnext/kirat_plastics_erpnext/doctype/excise_chapter/excise_chapter.py
+++ kiratplastics_erpnext/kirat_plastics_erpnext/doctype/excise_chapter/excise_chapter.py
@@ -8,3 +8,14 @@ from frappe.utils.nestedset import NestedSet
 
 class ExciseChapter(NestedSet):
 	nsm_parent_field = 'parent_excise_chapter'
+
+
+@frappe.whitelist()
+def update_items(excise_chapter, rate_of_duty):
+	items_to_update = frappe.get_all("Item", fields=["item_code"], filters={"excise_chapter": excise_chapter})
+	for item in items_to_update:
+		upd_itm = frappe.get_doc("Item", item.item_code)
+		upd_itm.excise_duty_rate = rate_of_duty
+		upd_itm.save()
+	frappe.db.commit()
+
