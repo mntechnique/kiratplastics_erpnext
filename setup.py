6385d156c24480ec71e4c4120a3db370c27e3133
Gaurav Naik <gvrvnk@gmail.com>
2016-07-30 18:59:24 +0530

    [Fix] setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 import re, ast
- 
- # get version from __version__ variable in erpnext/__init__.py
- _version_re = re.compile(r'__version__\s+=\s+(.*)')
- 
- with open('kiratplastics_erpnext/__init__.py', 'rb') as f:
-     version = str(ast.literal_eval(_version_re.search(
-         f.read().decode('utf-8')).group(1)))
+
+# get version from __version__ variable in erpnext/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('kiratplastics_erpnext/__init__.py', 'rb') as f:
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
 
 requirements = parse_requirements("requirements.txt", session="")
 
